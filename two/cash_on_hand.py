31793e58ca1eb059ec8a9e070fe5341a65fa1e80
Jk4403 <jktan2002@gmail.com>
2022-08-03 23:30:30 +0800

    My Changes

--- two/cash_on_hand.py
+++ two/cash_on_hand.py
@@ -1,2 +1,10 @@
 from pathlib import Path
 import csv
+import requests
+key_api = 'BW8DP6OHTV62MU57'
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE'
+
+responses = requests.get(url)
+
+import json
+data=responses.json()
