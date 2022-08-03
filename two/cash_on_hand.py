593571e55ad3a77382532e15cd95fb4e239b89c2
Jk4403 <jktan2002@gmail.com>
2022-08-03 23:39:53 +0800

    commit

--- two/cash_on_hand.py
+++ two/cash_on_hand.py
@@ -11,5 +11,14 @@ data=responses.json()
 print(json.dumps(data,indent=4))
 
 closed_data=data["REAL TIME CURRENCY EXCHANGE RATE"]
-exchange_rate=(float(closed_data['5. Exchange Rate']))
-fp=Path.cwd
+exchange_rate=(float(closed_data['5. exchange rate']))
+fp=Path.cwd()/"Two folder"
+file_path=Path.cwd()/"Two folder"/"cash-on-hand_usd.csv"
+#print(fp.exists())
+with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:
+    Readers = csv.reader(file)
+    next(Readers)
+    empty_list=[]
+    flag_list=[]
+    for line in Readers:
+        empty_list.append(line)
