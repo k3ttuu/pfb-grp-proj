39212d01c68e23c7fd6ad18c12f67b7ba9c44656
Jk4403 <jktan2002@gmail.com>
2022-08-03 23:55:32 +0800

    commit

--- two/cash_on_hand.py
+++ two/cash_on_hand.py
@@ -22,3 +22,23 @@ with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:
     flag_list=[]
     for line in Readers:
         empty_list.append(line)
+
+previous_figure = float(empty_list[0][1])
+days = empty_list
+for value in empty_list:
+    if float(value[1]) >= float(previous_figure):
+        previous_figure = float(value[1])
+    else:
+        diff = float(previous_figure) - float(value[1])
+        def convUSD_SGD(USD):
+            """
+        Fuction converts USD to SGD by multiplying exchange rate, this will return the coverted value
+            """
+            return USD * exchange_rate
+        SGD = (convUSD_SGD(USD = diff))
+        day=value[0]
+        prev_figure = float(value[1])
+        cash_on_hand = (day,SGD)
+        flag_list.append(cash_on_hand)
+        prev_figure = float(value[1])
+print(flag_list)
