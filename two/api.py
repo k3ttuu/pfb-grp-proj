f804cfe1630e835838f1ca9d3db9205e6927f75e
Jk4403 <jktan2002@gmail.com>
2022-08-04 00:40:39 +0800

    commit

--- two/api.py
+++ two/api.py
@@ -1,9 +1,9 @@
 import pip._vendor.requests as requests, json
 def currency_exchange_rate(from_currency,to_currency):
-    api_key = "BW8DP6OHTV62MU57"
+    api_key = "BC94UTRLKLJQB8ZA"
     from_currency = "USD"
     to_currency = "SGD"
-    base_url = r"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE"
+    base_url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={base_url}"
     main_url = base_url + "&from_currency=" + from_currency + "&to_currency=" + to_currency + "&apikey=" + api_key
     # print (main_url)
     response = requests.get(main_url)
--- two/cash_on_hand.py
+++ two/cash_on_hand.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import csv
 import requests
-key_api = 'BW8DP6OHTV62MU57'
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE'
+key_api = 'BC94UTRLKLJQB8ZA'
+url = ''
 
 responses = requests.get(url)
 
@@ -10,11 +10,10 @@ import json
 data=responses.json()
 print(json.dumps(data,indent=4))
 
-closed_data=data["REAL TIME CURRENCY EXCHANGE RATE"]
+closed_data=data["Real Time Currency Exchange Rate"]
 exchange_rate=(float(closed_data['5. exchange rate']))
 fp=Path.cwd()/"Two folder"
 file_path=Path.cwd()/"Two folder"/"cash-on-hand_usd.csv"
-#print(fp.exists())
 with file_path.open(mode = "r",encoding = "UTF-8", newline="") as file:
     Readers = csv.reader(file)
     next(Readers)
