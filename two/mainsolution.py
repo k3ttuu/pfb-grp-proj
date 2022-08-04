069ddcef18d27971c193fb18d98c2dabb2d2eca5
k3ttuu <s10243335@connect.np.edu.sg>
2022-08-04 16:33:37 +0800

    c

--- two/mainsolution.py
+++ two/mainsolution.py
@@ -1,7 +1,7 @@
 import api, cash_on_hand, profit_loss, overheads
 
 def main ():
-    forex = api.apifunction()
+    forex = api.currency_exchange_rate()
     overheads.overheads.function(forex)
     cash_on_hand.cash_on_hand.function(forex)
     profit_loss.profit.loss.function(forex)
